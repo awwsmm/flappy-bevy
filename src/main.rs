f4b7abf76b25748d089766ee46037bc7b01efa89
Andrew William Watson <aww@awwsmm.com>
2024-03-25 19:05:50 -0400

    step 3: render sprite in setup system

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,25 @@ fn main() {
 
 fn setup(
     mut commands: Commands,
+    asset_server: Res<AssetServer>,
 ) {
     commands.spawn(Camera2dBundle::default());
+    spawn_sprite(commands, asset_server);
+}
+
+fn spawn_sprite(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(
+        SpriteBundle {
+            texture: asset_server.load("bevy.png"), // 256x256
+            transform: Transform {
+                translation: Vec3::new(-400.0, 0.0, 0.0),
+                scale: Vec3::new(0.5, 0.5, 1.0), // 50% scale == 128x128
+                ..default()
+            },
+            ..default()
+        },
+    );
 }
