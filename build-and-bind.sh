63660990c5cc6249a04c1ee6fa90dd53ce17e1d0
Andrew William Watson <aww@awwsmm.com>
2024-03-25 21:12:45 -0400

    step 12: compile to WASM and play on the web

---
+++ build-and-bind.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# build
+cargo build --release --target wasm32-unknown-unknown
+
+# bind
+wasm-bindgen --out-dir target --target web target/wasm32-unknown-unknown/release/flappy-bevy.wasm
+
+# ...and then run with live-server
